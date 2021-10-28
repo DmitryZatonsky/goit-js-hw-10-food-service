3ad86bd80d15e968b6dac3180d40e84d9d608cb2
Dmitry Zatonsky <83658538+DmitryZatonsky@users.noreply.github.com>
2021-10-28 17:04:22 +0300

    finish

---
+++ src/index.js
@@ -0,0 +1,34 @@
+//Разметка меню по шаблону
+import menuData from './menu.json';
+import menuTemplates from './templates/menu.hbs';
+
+const menuListRef = document.querySelector('.js-menu');
+const menuMarkup = menuTemplates(menuData);
+
+menuListRef.insertAdjacentHTML('beforeend', menuMarkup);
+
+//Переключение тем
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body');
+const themeSwitcherRef = document.querySelector('#theme-switch-toggle');
+
+themeSwitcherRef.addEventListener('change', onCurrentTheme)
+
+bodyRef.classList.add(localStorage.getItem('Theme') ?? Theme.LIGHT);
+themeSwitcherRef.checked = localStorage.getItem('Theme') === Theme.DARK;
+
+function onCurrentTheme({ target: { checked } }) {
+  checked
+    ? togleTheme(Theme.DARK, Theme.LIGHT)
+    : togleTheme(Theme.LIGHT, Theme.DARK)
+};
+
+function togleTheme (add, rem) {
+  bodyRef.classList.add(add);
+  bodyRef.classList.remove(rem);
+  localStorage.setItem('Theme', add);
+};
